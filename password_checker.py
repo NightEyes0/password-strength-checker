b8ad90299ec6c21f5abaf4c478de91a64b7db07a
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-15 20:22:32 +0100

    Start password scoring system

--- password_checker.py
+++ password_checker.py
@@ -1,13 +1,11 @@
 password = input("Enter a password: ")
-
 length = len(password)
-
 has_uppercase = any(char.isupper() for char in password)
 has_lowercase = any(char.islower() for char in password)
 has_number = any(char.isdigit() for char in password)
-
 special_characters = "!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~"
 has_special = any(char in special_characters for char in password)
+score = 0
 
 print("\nPassword Analysis")
 print("-----------------")
