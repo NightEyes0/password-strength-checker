37029692d060af95c84e45794416c706530adfad
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-15 17:49:56 +0100

    Add password character checks

--- password_checker.py
+++ password_checker.py
@@ -2,4 +2,17 @@ password = input("Enter a password: ")
 
 length = len(password)
 
-print("Password length:", length)
+has_uppercase = any(char.isupper() for char in password)
+has_lowercase = any(char.islower() for char in password)
+has_number = any(char.isdigit() for char in password)
+
+special_characters = "!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~"
+has_special = any(char in special_characters for char in password)
+
+print("\nPassword Analysis")
+print("-----------------")
+print("Length:", length)
+print("Uppercase:", has_uppercase)
+print("Lowercase:", has_lowercase)
+print("Number:", has_number)
+print("Special character:", has_special)
