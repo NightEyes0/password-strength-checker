fd3e7cd21baf71775bbd8135361ecc598e90ddc1
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-15 21:46:58 +0100

    Add length scoring

--- password_checker.py
+++ password_checker.py
@@ -1,11 +1,17 @@
 password = input("Enter a password: ")
+
 length = len(password)
 has_uppercase = any(char.isupper() for char in password)
 has_lowercase = any(char.islower() for char in password)
 has_number = any(char.isdigit() for char in password)
+
 special_characters = "!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~"
 has_special = any(char in special_characters for char in password)
+
 score = 0
+
+if length >= 8:
+    score += 20
 
 print("\nPassword Analysis")
 print("-----------------")
@@ -14,3 +20,4 @@ print("Uppercase:", has_uppercase)
 print("Lowercase:", has_lowercase)
 print("Number:", has_number)
 print("Special character:", has_special)
+print("Score:", score)
